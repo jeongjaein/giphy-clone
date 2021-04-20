452b030048acfb42527addfb0910665b70d8be4f
jeongjaein <wodls96@gmail.com>
2021-04-21 02:24:16 +0900

    feat(...) : SubHeadingLabel 추가

--- GiphyClone/TabBar/ViewComponent/SubHeadingLabel.swift
+++ GiphyClone/TabBar/ViewComponent/SubHeadingLabel.swift
@@ -5,4 +5,19 @@
 //  Created by 정재인 on 2021/04/21.
 //
 
-import Foundation
+import UIKit
+
+class SubHeadingLabel: UILabel {
+    
+    init(_ frame: CGRect? = .zero) {
+        super.init(frame: frame!)
+        self.text = text
+        self.font = UIFont(name: "Apple SD Gothic Neo Regular", size: 15)
+        self.backgroundColor = .black
+        self.textColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
