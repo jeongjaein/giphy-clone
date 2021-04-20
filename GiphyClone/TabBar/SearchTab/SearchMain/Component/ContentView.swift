6668ec88839dd7211857372303482200db7e88ec
jeongjaein <wodls96@gmail.com>
2021-04-21 02:23:59 +0900

    feat(...) : ContentView 추가

---
+++ GiphyClone/TabBar/SearchTab/SearchMain/Component/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  GiphyClone
+//
+//  Created by 정재인 on 2021/04/21.
+//
+
+import UIKit
+
+class ContentView: UIView {
+    
+    let subHeadingLabel = SubHeadingLabel()
+    var contentView = UIView()
+    
+    init(frame: CGRect = .zero, content: UIView) {
+        super.init(frame: frame)
+        self.contentView = content
+        layout()
+    }
+    
+    func layout() {
+        [subHeadingLabel, contentView].forEach { addSubview($0) }
+        
+        subHeadingLabel.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                $0.topAnchor.constraint(equalTo: topAnchor),
+                $0.leadingAnchor.constraint(equalTo: leadingAnchor)
+            ])
+        }
+        contentView.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                $0.topAnchor.constraint(equalTo: subHeadingLabel.bottomAnchor,
+                                        constant: 5),
+                $0.leadingAnchor.constraint(equalTo: leadingAnchor),
+                $0.trailingAnchor.constraint(equalTo: trailingAnchor),
+                $0.bottomAnchor.constraint(equalTo: bottomAnchor)
+            ])
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
