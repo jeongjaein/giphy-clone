0abe951cafc02c8d4d20cda0ac98546994d2c318
jeongjaein <wodls96@gmail.com>
2021-04-21 03:27:37 +0900

    feat(...) : struct AutoComplete 추가

--- GiphyClone/TabBar/Entity/AutoComplete.swift
+++ GiphyClone/TabBar/Entity/AutoComplete.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+// MARK: - Welcome
+struct AutoComplete: Codable {
+    let data: [Datum]
+    let pagination: Pagination
+    let meta: Meta
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+    let name, analyticsResponsePayload: String
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case analyticsResponsePayload = "analytics_response_payload"
+    }
+}
+
+// MARK: - Meta
+struct Meta: Codable {
+    let status: Int
+    let msg, responseID: String
+
+    enum CodingKeys: String, CodingKey {
+        case status, msg
+        case responseID = "response_id"
+    }
+}
+
+// MARK: - Pagination
+struct Pagination: Codable {
+    let count, offset: Int
+}
