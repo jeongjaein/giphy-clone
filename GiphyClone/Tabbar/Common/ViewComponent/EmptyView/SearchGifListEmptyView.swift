f99de3124d454e96536c93f0d36ce4e8e64876df
jeongjaein <wodls96@gmail.com>
2021-04-26 04:26:29 +0900

    feat(...) : SearchGifListEmptyView 추가

--- GiphyClone/Tabbar/Common/ViewComponent/EmptyView/SearchGifListEmptyView.swift
+++ GiphyClone/Tabbar/Common/ViewComponent/EmptyView/SearchGifListEmptyView.swift
@@ -5,4 +5,15 @@
 //  Created by 정재인 on 2021/04/26.
 //
 
-import Foundation
+import UIKit
+
+class SearchGifListEmptyView:  BaseEmptyView {
+    override func attribute() {
+        iconImageView.do {
+            $0.setSFSymbols(systemName: "wind", weight: .bold)
+        }
+        guideLabel.do {
+            $0.text == "검색 결과가 없습니다."
+        }
+    }
+}
