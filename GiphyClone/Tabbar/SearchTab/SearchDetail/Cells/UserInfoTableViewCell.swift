b95b965525b77ab757071553265e81c8d38caa05
jeongjaein <wodls96@gmail.com>
2021-04-26 03:26:30 +0900

    refactor(...) : model 쓰는 데이터들만 정리

--- GiphyClone/Tabbar/SearchTab/SearchDetail/Cells/UserInfoTableViewCell.swift
+++ GiphyClone/Tabbar/SearchTab/SearchDetail/Cells/UserInfoTableViewCell.swift
@@ -114,11 +114,24 @@ extension UserInfoTableViewCell {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
         
+//        contentView.do {
+//            NSLayoutConstraint.activate([
+//                $0.topAnchor.constraint(equalTo: topAnchor),
+//                $0.heightAnchor.constraint(equalToConstant: 50),
+//                $0.bottomAnchor.constraint(equalTo: bottomAnchor)
+//            ])
+//        }
+        contentView.do {
+            NSLayoutConstraint.activate([
+                $0.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        }
         userProfileImageView.do {
             NSLayoutConstraint.activate([
-                $0.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-                $0.widthAnchor.constraint(equalToConstant: 50),
-                $0.heightAnchor.constraint(equalToConstant: 50),
+                $0.topAnchor.constraint(equalTo: contentView.topAnchor),
+//                $0.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                $0.widthAnchor.constraint(equalTo: contentView.heightAnchor),
+                $0.widthAnchor.constraint(equalTo: contentView.heightAnchor),
                 $0.leadingAnchor.constraint(
                     equalTo: leadingAnchor, constant: 10),
                 $0.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
--- GiphyClone/Tabbar/SearchTab/SearchMain/Component/SearchMainCustomTab.swift
+++ GiphyClone/Tabbar/SearchTab/SearchMain/Component/SearchMainCustomTab.swift
@@ -23,6 +23,16 @@ class SearchMainCustomTab: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    override func layoutIfNeeded() {
+        super.layoutIfNeeded()
+        
+    }
 }
 
 extension SearchMainCustomTab {
