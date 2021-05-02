73a4d417ca7247294074ca36b1ef6dd98c2c6b36
jeongjaein <wodls96@gmail.com>
2021-05-03 08:09:41 +0900

    collectionview 소폭수정

--- GiphyClone/Tabbar/Extensions/UICollectionView+Ext.swift
+++ GiphyClone/Tabbar/Extensions/UICollectionView+Ext.swift
@@ -9,7 +9,10 @@ import UIKit
 
 extension UICollectionView {
     func setEmptyView(type: EmptyViewType) {
-        type.view.frame = CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height)
+        type.view.frame = CGRect(x: self.center.x,
+                                 y: self.center.y,
+                                 width: self.bounds.size.width,
+                                 height: self.bounds.size.height)
         self.backgroundView = type.view
     }
 
